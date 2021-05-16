5b9f6a7d04000763cc87e809b15466a312e98abe
Kevin Hartman <kevin@hart.mn>
2021-05-16 01:46:58 -0400

    Update from PR.

--- src/Ubiquity.NET.Llvm/LLVMSharpExtensions/LLVMModuleFlagEntryExtensions.cs
+++ src/Ubiquity.NET.Llvm/LLVMSharpExtensions/LLVMModuleFlagEntryExtensions.cs
@@ -7,19 +7,22 @@ using System;
 
 namespace LLVMSharp.Interop
 {
-    /// <summary>Extensions for the LLVMSharp.Interop.LLVMDIBuilderRef class.</summary>
+    /// <summary>Extensions for <see cref="LLVMModuleFlagEntry"/>.</summary>
     public static unsafe class LLVMModuleFlagEntryExtensions
     {
+        /// <summary>Convenience wrapper for <see cref="LLVM.DisposeModuleFlagsMetadata"/>.</summary>
         public static void Dispose(this LLVMModuleFlagEntry entries)
         {
             LLVM.DisposeModuleFlagsMetadata(entries);
         }
 
+        /// <summary>Convenience wrapper for <see cref="LLVM.ModuleFlagEntriesGetFlagBehavior"/>.</summary>
         public static LLVMModuleFlagBehavior GetFlagBehavior(this LLVMModuleFlagEntry entries, uint index)
         {
             return LLVM.ModuleFlagEntriesGetFlagBehavior(entries, index);
         }
 
+        /// <summary>Convenience wrapper for <see cref="LLVM.ModuleFlagEntriesGetKey"/>.</summary>
         public static string GetKey(this LLVMModuleFlagEntry entries, uint index)
         {
             UIntPtr len;
@@ -33,6 +36,7 @@ namespace LLVMSharp.Interop
             return new ReadOnlySpan<byte>(pStr, (int)len).AsString();
         }
 
+        /// <summary>Convenience wrapper for <see cref="LLVM.ModuleFlagEntriesGetMetadata"/>.</summary>
         public static LLVMMetadataRef GetMetadata(this LLVMModuleFlagEntry entries, uint index)
         {
             return LLVM.ModuleFlagEntriesGetMetadata(entries, index);
